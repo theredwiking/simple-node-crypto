f02d7373395fba73d8d9169ab80b5dfcc0070b36
Niclas H <nihj@redship.redship.lan>
2022-04-28 08:53:52 +0200

    Version 0.0.4

--- components/hash.js
+++ components/hash.js
@@ -2,7 +2,7 @@ const { scryptSync, randomBytes } = require('crypto');
 
 const Hash = (password) => {
     const salt = randomBytes(16).toString('hex');
-    const hashed = scryptSync(password, salt, 64).toString('hex');
+    const hashed = scryptSync(password, salt, 64, { N: 16384, r: 8, p: 1 }).toString('hex');
 
     return {salt: salt, hash: hashed};
 };
--- components/verifyhash.js
+++ components/verifyhash.js
@@ -1,7 +1,7 @@
 const { scryptSync, timingSafeEqual } = require('crypto');
 
 const verifyHash = (password, salt, hashed) => {
-    const hashedBuffer = scryptSync(password, salt, 64);
+    const hashedBuffer = scryptSync(password, salt, 64, {N: 16384, r: 8, p: 1});
 
     const keyBuffer = Buffer.from(hashed, 'hex');
     const match = timingSafeEqual(hashedBuffer, keyBuffer);
